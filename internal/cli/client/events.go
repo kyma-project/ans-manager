745e1a26cacae1ec1d3c78036845cb393ddca378
Marcin Szwed <marcin.szwed@sap.com>
2025-10-15 18:07:53 +0200

    Add SendEvent method

--- internal/cli/client/events.go
+++ internal/cli/client/events.go
@@ -1,13 +1,18 @@
 package client
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/kyma-project/ans-manager/events"
 	"github.com/kyma-project/ans-manager/internal/cli/credentials"
 	"golang.org/x/oauth2/clientcredentials"
 )
+
+const eventsEndpoint = "/cf/producer/service/v1/resource-events"
 
 type EventsClient struct {
 	httpClient *http.Client
@@ -33,4 +38,32 @@ func NewEventsClient(region string, credentialsProvider credentials.Provider) (*
 		httpClient: httpClient,
 		serviceURL: creds.ServiceURL,
 	}, nil
+}
+
+func (c *EventsClient) SendEvent(event *events.ResourceEvent) error {
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("failed to marshal event: %w", err)
+	}
+
+	url := fmt.Sprintf("%s%s", c.serviceURL, eventsEndpoint)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(eventJSON))
+	if err != nil {
+		return fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send HTTP request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return fmt.Errorf("HTTP request failed with status %d: %s", resp.StatusCode, resp.Status)
+	}
+
+	return nil
 }
